bb94379764224eb7c28f82df5bd0865ac484dfc4
Kishan Tiwari <kishanmanitiwari@gmail.com>
2024-07-18 23:10:37 +0530

    Day_3

--- Day_3_JSX/App.jsx
+++ Day_3_JSX/App.jsx
@@ -15,16 +15,16 @@ import "./App.css";
 //   </div>
 // );
 
- const jsxHead = <h1>Hello</h1>
+const jsxHead = <h1>Hello</h1>;
 
-const name = "Hassan"; //JS Code 
+const name = "Hassan"; //JS Code
 
 //JSX-JS Samjhne - JS Mode Lana
 
 const cssObject = {
-  color:"blue",
-  fontSize:150,
-}
+  color: "blue",
+  fontSize: 150,
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Parent />);
