e512651a02e41ac242400afb80c6b8de924b6035
Kishan Tiwari <kishanmanitiwari@gmail.com>
2024-07-18 23:10:22 +0530

    Day_3_Code

---
+++ Day_3_JSX/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+
+//JSX
+
+// const jsxHeading = (<h1>Hello React</h1>)
+
+// const jsxParent = (
+//   <div id="parent">
+//     <div className="children"  >
+//       <h1 fontSize="50">Parent</h1>
+//       <h1>React!</h1>
+//     </div>
+//   </div>
+// );
+
+ const jsxHead = <h1>Hello</h1>
+
+const name = "Hassan"; //JS Code 
+
+//JSX-JS Samjhne - JS Mode Lana
+
+const cssObject = {
+  color:"blue",
+  fontSize:150,
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Parent />);
---
+++ Day_3_JSX/Parcel.md
@@ -0,0 +1,20 @@
+#Parcel(BEAST) - Features
+
+- Dev Build
+- Production Build
+- Local Server
+- HMR - Got Module Replacement
+- File Watching Algorithm - c++
+- Caching
+- Image Optimastion
+- Minification
+- Compression
+- Bundling
+- Consistent Hashing
+- Code Spliting
+- Diffrential Rendering
+- Compression
+- Dignostic
+- Error Handling
+- HTTP/HTTPS
+- Tree Shaking
