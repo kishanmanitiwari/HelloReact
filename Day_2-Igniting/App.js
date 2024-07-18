7cdb32789593a37a3077b7d65a1d48a077c8b225
Kishan Tiwari <kishanmanitiwari@gmail.com>
2024-07-18 14:12:10 +0530

    React Intro

--- Day_2-Igniting/App.js
+++ Day_2-Igniting/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // <div id = parent>
 //     <div id = children>
 //         <h1>Hello</h1>
@@ -5,19 +8,36 @@
 //     </div>
 // </div>
 
-const parent = React.createElement(
-  "div",
-  {id:"parent"},
-  React.createElement("div", {id:"children"},
- [React.createElement("h1", {}, "Hello"),React.createElement("h1",{},"World")])
-);
+// const parent = React.createElement(
+//   "div",
+//   {id:"parent"},
+//   React.createElement("div", {id:"children"},
+//  [React.createElement("h1", {}, "Hello"),React.createElement("h1",{},"React! from Kishan")])
+// );
 
-const heading = React.createElement(
-  "h1",
-  { id: "head", class: "heading" },
-  "Hello From React!"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "head", class: "heading" },
+//   "Hello From React!"
+// );
+
+// const parent = (
+//   <div>
+//     <h1>Hello</h1>
+//     {console.log("hello")}
+//   </div>
+// );
+
+function Parent() {
+  
+
+  return (
+    <div>
+      {console.log("hello")} 
+      <h1>Hello</h1>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<Parent />);
