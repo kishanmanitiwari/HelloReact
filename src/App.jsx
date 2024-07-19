e0012aabffde799ba2a3e727715009297f26c9b6
Kishan Tiwari <9164715-KishanTiwari@users.noreply.replit.com>
2024-07-19 09:15:28 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+import Note from "./components/Note";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+      <Note title="My Notes" content="This is first Note" />
+      <Note title="Rahul's Notes" content="This is second Note" />
+      <Note title="Ankit's Notes" content="This is second Note" />
+      <Note title="Ananya's Notes" content="This is your Notes" />
+      <Footer />
+    </main>
+  );
+}
